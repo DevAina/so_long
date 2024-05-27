4960173cbbb986861357d2f9afedfcd3fa0edfd3
DevAina <ainatolotriniavo7@gmail.com>
2024-05-27 08:59:59 +0300

    Graphique ok, toucheok

---
+++ test.c
@@ -0,0 +1,93 @@
+#include "minilibx-linux/mlx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/keysym.h>
+
+typedef struct s_mlx
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img;
+	void	*img_background;
+}	t_mlx;
+
+int	handle_key(int keysym, t_mlx *mlx)
+{
+	static int x;
+	static int y;
+	static int back;
+	static int back_y;
+	
+	if (keysym == XK_Escape)
+	{
+		printf("state escape: ON\n");
+		mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
+		mlx_destroy_image(mlx->mlx_ptr, mlx->img);
+		mlx_destroy_image(mlx->mlx_ptr, mlx->img_background);
+		mlx_destroy_display(mlx->mlx_ptr);
+		free(mlx->mlx_ptr);
+		//free(mlx->img);
+		exit(0);
+	}
+	printf("key: %d\n", keysym);
+	if (keysym == XK_d && x + 64 < 512)
+	{
+		if (x <= 0)
+			back = 0;
+		else
+			back = x;
+		x += 64;
+		mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img_background, back, y);
+		mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img, x, y);
+	}
+	else if (keysym == XK_q && x > 0 && x - 64 >= 0)
+	{
+		back = x;
+		x -= 64;
+		mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img_background, back, y);
+		mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img, x, y); 
+	}
+	else if (keysym == XK_s && y + 64 < 512)
+	{
+		back_y = y;
+		y += 64;
+		mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img_background, x, back_y);
+		mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img, x, y);
+	}
+	else if (keysym == XK_z && y > 0)
+	{
+		back_y = y;
+		y -= 64;
+		mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img_background, x, back_y);
+		mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr, mlx->img, x, y);
+	}
+	printf("Pos\nx: %d   y: %d\n", x, y);
+	return (0);
+}
+
+int	main()
+{
+	t_mlx mlx;
+	int	width;
+	int	height;
+	int	width_1;
+	int	height_1;
+
+	mlx.mlx_ptr = mlx_init();
+	if (mlx.mlx_ptr == NULL)
+		return (1);
+	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, 512, 512, "test");
+	if (mlx.win_ptr == NULL)
+	{
+		mlx_destroy_display(mlx.mlx_ptr);
+		free(mlx.mlx_ptr);
+		return (1);
+	}
+	mlx.img = mlx_xpm_file_to_image(mlx.mlx_ptr, "vaisseau.xpm", &width, &height);
+	printf("width: %d   heaight: %d\n", width, height);
+	mlx.img_background = mlx_xpm_file_to_image(mlx.mlx_ptr, "background.xpm", &width_1, &height_1);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.img, 0, 0);
+	mlx_key_hook(mlx.win_ptr, &handle_key, &mlx);
+	mlx_loop(mlx.mlx_ptr);
+	return (0);
+}
